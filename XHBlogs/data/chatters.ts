3abeedcceab41831e9de50577274da25505a7643
JWL956 <2425591445@qq.com>
2026-08-28 13:28:26 +0800

    add data/chatters.ts (在线管理台数据源)

---
+++ XHBlogs/data/chatters.ts
@@ -0,0 +1,14 @@
+// 🛡️ 本文件由在线管理台自动生成，请勿手动修改
+export interface Chatter { id: string; title: string; date: string; tags: string[]; mood: string; cover: string; description: string; content: string; }
+export const chatters: Chatter[] = [
+  {
+    "id": "chatter2",
+    "title": "横道世之介",
+    "date": "2026-08-25 12:00:00",
+    "tags": [],
+    "mood": "开心",
+    "cover": "https://raw.githubusercontent.com/JWL956/XinghuisamaBlogs/main/1111111.jpg",
+    "description": "",
+    "content": ""
+  }
+];
