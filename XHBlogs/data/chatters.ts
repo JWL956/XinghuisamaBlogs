c3c40c80052b51ccadc5230df3a861f0c94a82f4
JWL956 <2425591445@qq.com>
2026-08-28 13:28:53 +0800

    fix chatters.ts tags

--- XHBlogs/data/chatters.ts
+++ XHBlogs/data/chatters.ts
@@ -5,7 +5,7 @@ export const chatters: Chatter[] = [
     "id": "chatter2",
     "title": "横道世之介",
     "date": "2026-08-25 12:00:00",
-    "tags": [],
+    "tags": ["日常", "电影"],
     "mood": "开心",
     "cover": "https://raw.githubusercontent.com/JWL956/XinghuisamaBlogs/main/1111111.jpg",
     "description": "",
